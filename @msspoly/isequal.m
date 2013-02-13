16333bca79c3dce23b9dcabfca81b72ed4b4330c
mmt <mmt@992f7ab8-1a2d-4774-9c72-1763308a5dd4>
2013-02-13 20:39:26 +0000

    new isequal with tolerance.

--- @msspoly/isequal.m
+++ @msspoly/isequal.m
@@ -1,26 +1,35 @@
-function y=isequal(a,b)
-
-if ~isa(a,'msspoly') | ~isa(b,'msspoly') 
-    y = 0;
-elseif ~spot_hasSize(a,size(b)) 
-    y = 0;
-elseif isempty(a.coeff) & isempty(b.coeff)
-    y = 1;
-elseif ~spot_hasSize(a.coeff,size(b.coeff))
-    y = 0;
-elseif isempty(a.var)
-    if ~isempty(b.var), y = 0;
-    else
-        y =  (size(a.coeff,1) == size(b.coeff,1)) & ...
-             all(all([a.coeff] ==...
-                     [b.coeff]));
-    end
-elseif ~spot_hasSize(a.var,size(b.var))
-    y = 0;
-else
-    y =  (size(a.coeff,1) == size(b.coeff,1)) & ...
-         all(all([a.sub a.var a.pow a.coeff] ==...
-                 [b.sub b.var b.pow b.coeff]));
-end
+function y=isequal(a,b,tol)
+%
+%   y=isequal(a,b)
+%
+%   Returns y == logical(1)  if a and b are msspoly's
+%           of the same size and a-b is identically zero.
+%           y == logical(0) o.w.
+%
+%
+%   y=isequal(a,b,tol)
+%
+%   Returns y == logical(1) if a and b are of the same size
+%            and a-b has coefficients with absolute size less than
+%            tol.  If a-b cannot be computed an error is thrown.
+%
+%
+%
+  if ~spot_hasSize(a,size(b)) 
+      y = logical(0);
+  end
+  
+  if nargin < 3,
+      if ~isa(a,'msspoly') | ~isa(b,'msspoly') 
+          y = logical(0);
+          return;
+      else
+          tol = 0;
+      end
+  end
+  
+  diff = clean(a-b,tol);
+  
+  y = nnz(diff) == 0;
 
 end
---
+++ @msspoly/nnz.m
@@ -0,0 +1,3 @@
+function n = nnz(p)
+    n = length(find(p));
+end
