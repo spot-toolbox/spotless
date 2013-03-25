d72c03b837f7337f302e5d0b088d72d51eeaf93a
mmt <mmt@992f7ab8-1a2d-4774-9c72-1763308a5dd4>
2013-03-25 17:10:46 +0000

    updated for trig. poly

--- @msspoly/deg.m
+++ @msspoly/deg.m
@@ -5,5 +5,5 @@ if nargin < 2
 else
     if ~msspoly.isfreemsspoly(x), error('2nd argument must be free msspoly'); end
     [~,xn] = isfree(x);
-    d=max(sum((msspoly.match_list(xn(:),p.var)>0).*p.pow,2));
+    d=max(sum(abs((msspoly.match_list(xn(:),p.var)>0).*p.pow),2));
 end
