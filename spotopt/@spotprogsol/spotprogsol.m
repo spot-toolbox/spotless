b9ed72ccc7229671445f07195a3679da133bd08a
Mark M. Tobenkin <mmt@alum.mit.edu>
2013-05-07 08:24:53 -0400

    added dualEval and dimacs errors.

--- spotopt/@spotprogsol/spotprogsol.m
+++ spotopt/@spotprogsol/spotprogsol.m
@@ -12,12 +12,13 @@ classdef spotprogsol
         z = [];
         info = struct();
         prog = [];
+        objective = [];
         
         variables = [];
     end
     
     methods
-        function sol = spotprogsol(prog,x,y,z,info)
+        function sol = spotprogsol(prog,objective,x,y,z,info)
             if ~prog.isPrimalWithFree()
                 error(['Solutions must come from primal form with ' ...
                        'free variables right now.']);
@@ -27,10 +28,27 @@ classdef spotprogsol
             sol.y = y;
             sol.z = z;
             sol.info = info;
+            sol.objective = objective;
+        end
+        
+        function err = dimacs(sol)
+            [P,A,b,c,K,d] = sol.prog.toSedumi(sol.objective);
+            err = spot_sdp_dimacs(A,b,c,K,P'*sol.x,sol.y,P'*sol.z);
         end
         
         function e = eval(sol,expr)
-            e = subs(expr,sol.prog.variables,sol.prog.decToVar(sol.x));
+            e = subs(expr,...
+                     sol.prog.variables,...
+                     sol.prog.decToVar(sol.x));
+        end
+        
+        function e = dualEval(sol,expr)
+            nf = sol.prog.numFree;
+            e = subs(expr,...
+                     [sol.prog.variables
+                      sol.prog.dualEqVariables],...
+                     [sol.prog.decToVar([zeros(nf,1);sol.z])
+                      sol.y]);
         end
     end
 end
