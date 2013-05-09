50672ff3a8850dc758c502ee73a9f56e9e093304
Mark M. Tobenkin <mmt@alum.mit.edu>
2013-05-09 10:23:49 -0400

    simple test for dual recovery

---
+++ spotopt/tests/testCstr/psdTest3.m
@@ -0,0 +1,13 @@
+function [pr,obj,zero,tol] = psdTest3(pr)
+%
+%  Evalutate the nuclear norm of a matrix, posed
+%  in dual form.
+%
+    [pr,f] = pr.newFree(1);
+    [pr,z] = pr.withPSD([1 f ; f 1]);
+    
+    obj = f;
+    
+    zero = z(1,2) - (-1);
+    tol  = 1e-8;
+end
