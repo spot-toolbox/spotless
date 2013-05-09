c3b81674929d0bddc7b364afb7591b17e5ecf94b
Mark M. Tobenkin <mmt@alum.mit.edu>
2013-05-09 10:22:23 -0400

    sedumi needs to symmetrize arguments.

---
+++ spotopt/util/spot_sdp_cone_symm.m
@@ -0,0 +1,34 @@
+function xprime = spot_sdp_cone_symm(x,K);
+    [n,nf,nl,nq,nr,ns] = spot_sdp_cone_dim(K);
+    
+    if ~spot_hasSize(x,[n 1])
+        error('First argument must be a column of appropriate length.');
+    end
+    
+    if issparse(x),
+        xprime = sparse(size(x,1),size(x,2));
+    else
+        xprime = zeros(size(x));
+    end
+    
+    nid = nf+nl+nq+nr;
+    xprime(1:nid) = x(1:nid);
+    
+    if ns == 0, return; end
+    
+    sMap = containers.Map('KeyType','uint32','ValueType','any');
+    sz = unique(K.s);
+    for i = 1:length(sz)
+        n = sz(i);
+        T = reshape(1:n^2,n,n)';
+        I = speye(n^2);
+        sMap(n) = (I + sparse(T(:),(1:n^2),ones(n^2,1)))/2;
+    end
+    
+    maps = cell(length(K.s),1);
+    for i = 1:length(K.s)
+        maps{i} = sMap(K.s(i));
+    end
+    T = blkdiag(maps{:});
+    xprime(nid+1:end) = T*x(nid+1:end);
+end
--- spotopt/util/spot_sedumi.m
+++ spotopt/util/spot_sedumi.m
@@ -1,6 +1,6 @@
 function [x,y,z,info] = spot_sedumi(A,b,c,K)
     [x,y,s_info] = sedumi(A,b,c,K,struct('fid',0));
-    z = c - A'*y;
+    z = spot_sdp_cone_symm(c - A'*y,K);
     
     info.primalInfeasible = s_info.pinf;
     info.dualInfeasible   = s_info.dinf;
