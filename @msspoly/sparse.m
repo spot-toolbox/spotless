ab8bbd997851a33581386429155cc8fa952d3e03
Mark M. Tobenkin <mmt@alum.mit.edu>
2013-05-17 20:27:52 -0400

    update sparse to match MATLAB notation.

--- @msspoly/sparse.m
+++ @msspoly/sparse.m
@@ -1,30 +1,44 @@
-function P=sparse(p)
-% function P=sparse(p)
-%
-% for an msspoly matrix p=p(y) produces sparse matrix P of
-% minimal dimension such that p(y)x=0 for all y iff Px=0
+function varargout=sparse(varargin)
 
-% AM 11.01.09
-
-if isempty(p.coeff)
-    P = sparse([],[],[],0,p.dim(2));
-elseif size(p.coeff,1) == 1
-    P=sparse(1,p.sub(1,2),p.coeff(1),1,p.dim(2));
-else
-    [s,i] = sortrows([p.sub(:,1) p.var p.pow]);
-    j = p.sub(i,2);
-    coeff = p.coeff(i);
-    m = size(p.sub,1);
-    row = cumsum([1;1-all(s(1:m-1,:)==s(2:m,:),2)]);
-    P = sparse(row,j,coeff,row(end),p.dim(2));
-end
+if nargin == 1 
+    if nargout == 1
+        varargout{1} = msspoly(varargin{1});
+    else
+        [x,p,M] = decomp(varargin{1});
+        ind = find(~all(M==0,2));
+        [varargout{1},varargout{2}] = ind2sub(size(varargin{1}),ind);
+        varargout{3} = recomp(x,p,M(ind,:));
+    end
+elseif nargin == 2
+    varargout{1} = msspoly([varargin{1} varargin{2}],...
+                           zeros(0,2),zeros(0,1),zeros(0,1),...
+                           zeros(0,1));
     
+elseif nargin == 3 || nargin == 5
+    i = varargin{1};
+    j = varargin{2};
+    s = varargin{3};
+    if nargin == 3,
+        m = max(i); 
+        n = max(j);
+    else
+        m = varargin{4}; 
+        n = varargin{5};
+    end
+    [x,pow,M] = decomp(s);
+    [~,xid] = isfree(x);
+    [r,c,coeff] = find(M);
+    
+    N = length(coeff);
+    
+    dim = [ m n ];
+    sub = [ i(r) j(r) ];
+    var = repmat([ xid' ],N,1); % problem later for chordal sparsity.
+    pow = [ pow(c,:) ];
 
-% if isempty(p.s), P=sparse([],[],[],0,p.n); return; end
-% [ms,ns]=size(p.s);
-% if ms==1, P=sparse(1,p.s(1,2),p.s(1,ns),1,p.n); return; end
-% [ss,ii]=sortrows([p.s(:,1) p.s(:,3:ns-1)]);
-% jj=p.s(ii,2);
-% cc=p.s(ii,ns);
-% ee=cumsum([1;1-all(ss(1:ms-1,:)==ss(2:ms,:),2)]);
-% P=sparse(ee,jj,cc,ee(length(ee)),p.n);
+    varargout{1} = msspoly(dim,sub,var,pow,coeff);
+else
+    error('Unsupported number of arguments.');
+end
+
+end
