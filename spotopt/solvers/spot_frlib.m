952184c39eee44345167a59853f67a05d8abe391
frankpermenter <fperment@mit.edu>
2015-04-16 18:36:05 -0400

    Solver file for frlib

---
+++ spotopt/solvers/spot_frlib.m
@@ -0,0 +1,17 @@
+function [x,y,z,info] = spot_frlib(A,b,c,K,options)
+
+usefrlib =  exist('frlibPrg','class');
+if usefrlib
+    f = frlibPrg(A,b,c,K);
+    if ~isfield(options,'approx')
+        options.approx = 'd';
+    end
+    r = f.ReducePrimal(options.approx);
+    r.PrintStats();
+    [xr,yr,info] = f.Solve();
+    z = [];
+    
+    [x,y] = r.Recover(xr,yr);
+else
+    error('frlib not found!')
+end
