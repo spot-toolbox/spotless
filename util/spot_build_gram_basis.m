756436f541ec872d8a79ea52f725f3cba959be96
frankpermenter <fperment@mit.edu>
2015-12-10 12:54:52 -0500

    Block diagonalization code (Permenter, Parrilo CDC 2015)

--- util/spot_build_gram_basis.m
+++ util/spot_build_gram_basis.m
@@ -1,4 +1,4 @@
-function [mpow,coeffIndxPos,coeffIndxZero] = spot_build_gram_basis(pow,mpow)
+function [m_partition,coeffIndxPos,coeffIndxZero] = spot_build_gram_basis(pow,mpow)
   
     pow = full(pow);
     
@@ -9,13 +9,11 @@ function [mpow,coeffIndxPos,coeffIndxZero] = spot_build_gram_basis(pow,mpow)
     end
        
     numHyperPlanes = 2000;
-    [mpow] = RandomPrune(pow,mpow,numHyperPlanes);
-    
+    [mpow] = RandomPrune(pow,mpow,numHyperPlanes);  
     [mpow,coeffIndxPos,coeffIndxZero] = DiagConsistent(pow,mpow);
-
+    [~,m_partition] = BlockDiagonalize(pow,mpow);
+ 
 end
-    
-  
     
 %Remove monomials m if 2*m is outside convhull(pow) using
 %2*numHyperPlanes random seperating hyperplanes
@@ -105,7 +103,6 @@ end
 %Compute A+A, excluding term A_i + A_j if i = j
 function [S] = CrossTerms(A)
 
-
     numTerm = size(A,1);
     numVar = size(A,2);
     
@@ -137,30 +134,130 @@ function [S] = MinkSum(A)
     numTerm = size(A,1);
     numVar = size(A,2);
     
-    %Get index position of upper triangular part
-    posKeep = find(triu(ones(numTerm,numTerm,'uint8'),0));
+    %Compute A(i,:)+A(j,:) for all i, j \in 1..numTerm
+    S = zeros(numTerm*numTerm,numVar,class(A)); %#ok<ZEROLIKE>
 
-    if ~isempty(posKeep)
-        %Compute A(i,:)+A(j,:) for all i, j \in 1..numTerm
-        S=zeros(length(posKeep),numVar,class(A));
-
-        for k=1:numVar
-           temp=bsxfun(@plus,A(:,k),A(:,k)');
-           S(:,k) = temp(posKeep);
-        end
-        
-    else
-        S = [];
+    for k=1:numVar
+       temp=bsxfun(@plus,A(:,k),A(:,k)');
+       S(:,k) = temp(:);
     end
+        
+end
 
-    %Remove duplicates
-    [S]=unique(S,'rows');
+
+function [pow,m_partition] = BlockDiagonalize(pow,mpow)
+
+    n = size(mpow,1);
+    [minkSum] = MinkSum(mpow);
+  
+    M = zeros(n);
+    while (1)
+
+        size_pow = size(pow,1);
+        [indxKeep] = find(ismember(minkSum,pow,'rows'));
+        M(indxKeep) = 1;
+        [M,partition] = TransitiveClosure(M);
+        pow = minkSum(M(:) == 1,:);
+        if size(pow,1) == size_pow, break, end
+        
+    end
+  
+    for i = 1:length(partition)
+        m_partition{i} = mpow( partition{i},:);
+    end
+  
 
 end
 
 
 
+function [M,cliques] = TransitiveClosure(M)
+
+[r,~] = find(M);
+if isempty(r)
+    cliques = {}; return
+end
+
+r = unique(r);
+[~,~,cliques] = conncomp(M(r,r));
+
+for i=1:length(cliques)
+    cliques{i} = r(cliques{i});
+    M(cliques{i},cliques{i}) = 1;
+end
+
+end
+
+
+function [nComponents,sizes,members] = conncomp(A)
+% Number of nodes
+N = size(A,1);
+% Remove diagonals
+A(1:N+1:end) = 0;
+% make symmetric, just in case it isn't
+A=A+A';
+% Have we visited a particular node yet?
+isDiscovered = zeros(N,1);
+% Empty members cell
+members = {};
+% check every node
+for n=1:N
+    if ~isDiscovered(n)
+        % started a new group so add it to members
+        members{end+1} = n;
+        % account for discovering n
+        isDiscovered(n) = 1;
+        % set the ptr to 1
+        ptr = 1;
+        while (ptr <= length(members{end}))
+            % find neighbors
+            nbrs = find(A(:,members{end}(ptr)));
+            % here are the neighbors that are undiscovered
+            newNbrs = nbrs(isDiscovered(nbrs)==0);
+            % we can now mark them as discovered
+            isDiscovered(newNbrs) = 1;
+            % add them to member list
+            members{end}(end+1:end+length(newNbrs)) = newNbrs;
+            % increment ptr so we check the next member of this component
+            ptr = ptr+1;
+        end
+    end
+end
+% number of components
+nComponents = length(members);
+for n=1:nComponents
+    % compute sizes of components
+    sizes(n) = length(members{n});
+end
+
+[sizes,idx] = sort(sizes,'ascend');
+members = members(idx);
 
 
 
+% Copyright (c) 2014, Daniel Larremore
+% All rights reserved.
+% 
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+% 
+%     * Redistributions of source code must retain the above copyright
+%       notice, this list of conditions and the following disclaimer.
+%     * Redistributions in binary form must reproduce the above copyright
+%       notice, this list of conditions and the following disclaimer in
+%       the documentation and/or other materials provided with the distribution
+% 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+% LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+% POSSIBILITY OF SUCH DAMAGE.
+end
 
